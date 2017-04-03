4f146979b9a790e642cf31dfb401f36571556df7
kculafic <kculafic@gmail.com>
2017-04-03 13:04:48 -0700

    Clean up

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,14 +2,3 @@ import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 export default FirebaseAdapter.extend({
 });
-
-
-// import Ember from 'ember';
-// import FirebaseAdapter from 'emberfire/adapters/firebase';
-//
-// const { inject } = Ember;
-//
-// export default FirebaseAdapter.extend({
-//   firebase: inject.service(),
-//
-// });
